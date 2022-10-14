c4be2aba61ccae9179110ccfc693bcab535f2ecf
Muhammad Hussain <muhammad.hussain@datarovers.com>
2022-10-14 18:43:57 +0500

    Middlewares API Creation

---
+++ APIMiddlewares/Controllers/StudentController.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using PortalInterfaces.IPortalRepositories;
+using PortalModels.ResponceModels;
+using System;
+using System.Linq;
+using PortalMappers.PersonMappers;
+using System.Threading.Tasks;
+
+namespace APIMiddlewares.Controllers
+{
+    [Route("Middlewares/")]
+    [ApiController]
+    public class StudentController : ControllerBase
+    {
+        private readonly IStudentRepository repo;
+
+        public StudentController(IStudentRepository repo)
+        {
+            this.repo = repo;
+        }
+
+        #region Student MiddleWares
+
+        [HttpGet]
+        [Route("RetriveStudents-Program")]
+        [ResponseCache(Duration = 3600, Location = ResponseCacheLocation.Client, VaryByHeader = "User-Agent")]
+        public IActionResult GetStudent()
+        {
+            try
+            {
+                var modelList = repo.GetStudents().Select(x => x.ToModel());
+                if (modelList != null)
+                {
+                    return Ok( new Result<dynamic>
+                    {
+                        data = modelList,
+                        message = "Students list fetched Successfully",
+                        success = true
+                    });
+                }
+                else
+                    return BadRequest(new Result<dynamic> { message = "Students list not found" });
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(new Result<dynamic>
+                {
+                    success = true,
+                    message = ex.Message,
+                    data = null
+                });
+            }
+        }
+
+
+        #endregion
+
+    }
+}
--- APIMiddlewares/Startup.cs
+++ APIMiddlewares/Startup.cs
@@ -1,17 +1,14 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using Portal.DAL;
+using PortalInterfaces.IPortalRepositories;
+using PortalRepositories.PortalRepository;
+
 
 namespace APIMiddlewares
 {
@@ -35,10 +32,14 @@ namespace APIMiddlewares
                 });
             });
             services.AddControllers();
-            //services.AddSwaggerGen(c =>
-            //{
-            //    c.SwaggerDoc("v1", new OpenApiInfo { Title = "APIMiddlewares", Version = "v1" });
-            //});
+            services.AddScoped<PortalContext>();
+            services.AddScoped<IStudentRepository, StudentRepository>();
+
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "EIMS_API_Middleware", Version = "v1" });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -47,8 +48,8 @@ namespace APIMiddlewares
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-                //app.UseSwagger();
-                //app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "APIMiddlewares v1"));
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "APIMiddlewares v1"));
             }
 
             app.UseHttpsRedirection();
@@ -62,6 +63,7 @@ namespace APIMiddlewares
                 endpoints.MapControllers();
             });
 
+            
             app.Run(async (context) =>
             {
                 await context.Response.WriteAsync("Welcome to EIMS API Home.");
